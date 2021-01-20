a033b2dfd3c0a195c0d603ee850fc5857c1f4e00
KaweckiBartek <67326141+KaweckiBartek@users.noreply.github.com>
2021-01-20 15:38:16 +0100

    home ready

---
+++ quiz-app/src/components/buttons/CloseButton.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import closeIcon from "../../images/icons/zamknij_x.svg"
+
+const CloseButton = () => {
+  return (
+    <img className="closeButton" src={closeIcon} alt="close-button"/>
+  )
+}
+
+export default CloseButton
--- quiz-app/src/pages/index.tsx
+++ quiz-app/src/pages/index.tsx
@@ -1,16 +1,22 @@
-import React from "react"
-import Categories from "../components/categories";
-import QLogo from "../components/logo/QLogo";
-import QUIZLogo from "../components/logo/QUIZLogo";
+import React from 'react'
 import '../styles/themes/default/theme.scss';
+import Header from '../components/heading/Header'
+import QLogo from '../components/logo/QLogo'
+import QUIZLogo from '../components/logo/QUIZLogo'
+import Categories from '../components/categories'
+import CloseButton from '../components/buttons/closeButton';
 
-
-export default function Home() {
-  return <div className="home">
-    <div className="home__header">
-      <QLogo />
-      <QUIZLogo />
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="home__header">
+        <QLogo />
+        <QUIZLogo />
+        <CloseButton />
+      </div>
+      <Categories />
     </div>
-    <Categories />
-  </div>
+  )
 }
+
+export default Home
