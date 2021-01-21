31dce43946655daec05db5cb194cb3e0a9fef9ce
KaweckiBartek <67326141+KaweckiBartek@users.noreply.github.com>
2021-01-21 19:26:50 +0100

    added netlify plugin

--- quiz-app/gatsby-config.js
+++ quiz-app/gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
     'gatsby-transformer-sharp',
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-inline-svg`,
+    `gatsby-plugin-netlify`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
